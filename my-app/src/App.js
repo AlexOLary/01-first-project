b919b29aea6c953fc00f7328d49f89ccb2b8427d
AlexOLary <AlexOLary@gmail.com>
2020-05-12 03:19:28 +0700

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,8 +24,7 @@ const App = (props) => {
                 <Route path='/Profile'
                        render={() => <Profile
                            profilePage={props.state.profilePage}
-                           addPost={props.addPost}
-                           updateNewPostText = {props.updateNewPostText}/>}/>
+                           dispatch={props.dispatch}/>}/>
 
                 <Route path='/News' render={() => <News/>}/>
                 {/*<Route path='/Music' render={ () => <Music/>}/>
