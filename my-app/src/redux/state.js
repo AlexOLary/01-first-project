422388597f23f2cfcf9d9fed43f5047e492c2966
AlexOLary <AlexOLary@gmail.com>
2020-05-13 04:21:32 +0700

    commit

--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,5 +1,7 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 
 let store = {
     _state: {
@@ -14,6 +16,14 @@ let store = {
             newPostText: 'Введите ваше сообщение!'
         },
         dialogsPage: {
+            dialogs_data: [
+                {id: 1, name: 'Alex'},
+                {id: 2, name: 'Lex'},
+                {id: 3, name: 'Supermen'},
+                {id: 4, name: 'Misha'},
+                {id: 5, name: 'Vladimir'},
+                {id: 6, name: 'Fiona'},
+            ],
             messages_data: [
                 {id: 1, message: 'Hi'},
                 {id: 2, message: 'How is your www?'},
@@ -22,15 +32,9 @@ let store = {
                 {id: 5, message: 'Ho'},
                 {id: 6, message: 'Ho'},
             ],
-            dialogs_data: [
-                {id: 1, name: 'Alex'},
-                {id: 2, name: 'Lex'},
-                {id: 3, name: 'Supermen'},
-                {id: 4, name: 'Misha'},
-                {id: 5, name: 'Vladimir'},
-                {id: 6, name: 'Fiona'},
-            ]
+            newMessageBody: ''
         },
+        sidebar: {}
     },
     _callSubscriber() {
         console.log('State changed');
@@ -44,7 +48,7 @@ let store = {
     },
 
     dispatch(action){
-        if (action.type === 'ADD-POST'){
+        if (action.type === ADD_POST){
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -54,8 +58,16 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+        } else if (action.type === UPDATE_NEW_POST_TEXT){
             this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY){
+            this._state.dialogsPage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE){
+            let body = this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages_data.push({id: 7, message: body});
             this._callSubscriber(this._state);
         }
     }
@@ -63,10 +75,13 @@ let store = {
 
 
 
-export const addPostActionCreator = () => ({type: ADD_POST})
-
+export const addPostActionCreator = () => ({type: ADD_POST});
 export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text})
+    ({type: UPDATE_NEW_POST_TEXT, newText: text});
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) =>
+    ({type: UPDATE_NEW_MESSAGE_BODY, body: body});
 
 export default store;
 window.store = store;
