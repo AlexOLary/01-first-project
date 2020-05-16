791ee16c83f0f77eb7da892616920d51161fba9a
AlexOLary <AlexOLary@gmail.com>
2020-05-17 04:14:08 +0700

    commit

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -35,13 +35,8 @@ let Users = (props) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {
-                                    props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    props.follow(u.id)
-                                }}>Follow</button>
-                            }
+                                ? <button onClick={() => {props.unfollow(u.id) }}>Unfollow</button>
+                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
                         </div>
                     </span>
                     <span>
