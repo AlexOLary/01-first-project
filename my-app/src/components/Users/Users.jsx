8e7e3a075978ace268ee3c58a7cdfccfcfd6ef4c
AlexOLary <AlexOLary@gmail.com>
2020-05-23 23:25:03 +0700

    commit

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -5,9 +5,19 @@ import userPhoto from '../../assets/images/users.jpg'
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage} &count=${this.props.pageSize}`)
+            .then(response => {
             this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
         })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber} &count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
 
     render() {
@@ -23,7 +33,8 @@ class Users extends React.Component {
             <div>
                 <div>
                     {pages.map( p => {
-                       return <span className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
+                       return <span className={this.props.currentPage === p && styles.selectedPage}
+                       onClick={(e) => {this.onPageChanged(p);}}>{p}</span>
                     })}
                 </div>
                 {
