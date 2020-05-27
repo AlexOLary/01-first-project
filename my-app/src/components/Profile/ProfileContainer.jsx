263a1f5f1a17d267c0ca71c60cf8da968b9b4894
AlexOLary <AlexOLary@gmail.com>
2020-05-27 09:44:24 +0700

    commit

--- my-app/src/components/Profile/ProfileContainer.jsx
+++ my-app/src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import withRouter from "react-router/modules/withRouter";
 
 class ProfileContainer extends React.Component{
 
@@ -24,5 +25,6 @@ class ProfileContainer extends React.Component{
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 });
+let WithUrlDataContainerComponent = withRouter (ProfileContainer);
 
-export default connect(mapStateToProps,{setUserProfile}) (ProfileContainer);
+export default connect(mapStateToProps,{setUserProfile}) (WithUrlDataContainerComponent);
