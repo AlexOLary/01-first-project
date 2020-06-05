5afffb5361b21cf71d0ad5048aad97a09e233c9b
AlexOLary <AlexOLary@gmail.com>
2020-06-05 20:51:52 +0700

    commit

--- my-app/src/components/Users/Users.js
+++ my-app/src/components/Users/Users.js
@@ -36,7 +36,7 @@ let Users = (props) => {
                             {u.followed
                                 ? <button onClick={() => {
 
-                                    axios.delate(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
                                         {
                                             withCredentials: true,
                                             headers: {
