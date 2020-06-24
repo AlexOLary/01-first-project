4f8894f524bfbd1d0a0b56ac374c5850ecc2f1ca
AlexOLary <AlexOLary@gmail.com>
2020-06-24 23:51:41 +0700

    commit

--- my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,19 +3,26 @@ import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 
 class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    }
+
     render() {
         /*if (!props.profile) {
             return <Preloader/>
         }*/
         return (
             <div>
+                {!this.state.editMode &&
                 <div>
                     <span>{this.props.status}</span>
                 </div>
+                }
+                {this.state.editMode &&
                 <div>
                     <input value={this.props.status}/>
                 </div>
-
+                }
             </div>
         )
     }
