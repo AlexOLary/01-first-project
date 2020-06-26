f7bee7ddee0bf36785effbfd02fd4d6b6655f8f1
AlexOLary <AlexOLary@gmail.com>
2020-06-27 00:45:34 +0700

    redux-form // (login) the beginning of the form

--- my-app/src/components/Login/Login.jsx
+++ my-app/src/components/Login/Login.jsx
@@ -1,23 +1,37 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
 
-const Login = (props) => {
-    return <div>
-        <h1>LOGIN</h1>
-        <form action="">
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
             <div>
-                <input placeholder={"Login"}/>
+                {/*<label htmlFor="Login">Login</label>*/}
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
             </div>
             <div>
-                <input placeholder={"Password"}/>
+                {/*<label htmlFor="Password">Password</label>*/}
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
             </div>
             <div>
-                <input type={"checkbox"}/> remember me
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
             </div>
             <div>
                 <button>Login</button>
             </div>
         </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'Login'})(LoginForm)
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
     </div>
 }
 
